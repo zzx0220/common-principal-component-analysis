9f0e95ab29d1e501eadaab8f641317ae3fc70f89
zzx <zzx0220@126.com>
2025-12-25 20:49:44 +0800

    use a result structure like fa_across_conds

--- common_eig.m
+++ common_eig.m
@@ -23,31 +23,35 @@ function eigen_result = common_eig(covmat, options)
         options.manopt = struct();
     end
 
+    condn = size(covmat,3); % number of conditions
+
     % first we do eigenvalue decomposition separately to judge whether we could
     % find common eigenvectors
-    [v1,d1] = eig(covmat(:,:,1));
-    [v2,d2] = eig(covmat(:,:,2));
-    r1 = rank(covmat(:,:,1)); r2 = rank(covmat(:,:,2));
-    [d1,idx] = sort(diag(d1),'descend');
-    v1 = v1(:,idx);
-    [d2,idx] = sort(diag(d2),'descend');
-    v2 = v2(:,idx);
+
+    v = zeros(size(covmat,1),size(covmat,1),condn); % eigenvectors
+    d = zeros(size(covmat,1),condn); % eigenvalues
+    r = zeros(1,condn); % rank of the covariance matrix
+    for i = 1:condn
+        [v(:,:,i),tempd] = eig(covmat(:,:,i));
+        r(i) = rank(covmat(:,:,i));
+        [d(:,i),idx] = sort(diag(tempd),'descend');
+        v(:,:,i) = v(:,idx,i);
+    end
 
     % calculate the inner product (angle) between the eigenvectors
-    eig_sim = v1' * v2;
-    figure;
-    subplot(1,3,1); stem(d1(1:r1));title('eigenvalues of covmat 1');
-    subplot(1,3,2); stem(d2(1:r2));title('eigenvalues of covmat 2');
-    subplot(1,3,3); imagesc(eig_sim(1:r1,1:r2));clim([0 1]); title('Inner product between the eigenvectors'); % visually identify common vectors
-    drawnow;
+    % eig_sim = v1' * v2;
+    % figure;
+    % subplot(1,3,1); stem(d1(1:r1));title('eigenvalues of covmat 1');
+    % subplot(1,3,2); stem(d2(1:r2));title('eigenvalues of covmat 2');
+    % subplot(1,3,3); imagesc(eig_sim(1:r1,1:r2));clim([0 1]); title('Inner product between the eigenvectors'); % visually identify common vectors
+    % drawnow;
     
     % after confirming 
-    p = size(covmat,1); r = min([r1,r2]); % only consider the top r eigenvectors to reduce computation time
+    p = size(covmat,1); r = min(r); % only consider the top r eigenvectors to reduce computation time
 
     % Create the problem structure.
     elements.Q = stiefelfactory(p, r, 1);
-    elements.D1 = positivefactory(r,1);
-    elements.D2 = positivefactory(r,1);
+    elements.D = positivefactory(r,condn);
     manifold = productmanifold(elements);
     problem.M = manifold;
 
@@ -56,15 +60,14 @@ function eigen_result = common_eig(covmat, options)
     problem.egrad = @(x) egrad_func(x, covmat, options.sample_n);
 
     % Numerically check gradient consistency (just once, optional).
-    %checkgradient(problem); pause;
+    % checkgradient(problem); %pause;
 
     % use the eigenvector of the mean covariance matrix as initial point
     [v_avg,d_avg] = eig(mean(covmat,3));
     [d_avg,idx] = sort(diag(d_avg),'descend');
     v_avg = v_avg(:,idx);
     init_point.Q = v_avg(:,1:r);
-    init_point.D1 = sqrt(d_avg(1:r));
-    init_point.D2 = sqrt(d_avg(1:r));
+    init_point.D = repmat(sqrt(d_avg(1:r)),1,condn);
 
     % Solve.
     [eigen_result, cost, info, options] = trustregions(problem, init_point, options.manopt);
@@ -75,20 +78,32 @@ end
 function output = cost_func(param, covmat, sample_n)
     % this function is to calculate the cost of the problem.
 
-    output = sample_n(1) * norm(covmat(:,:,1) - (param.Q .* (param.D1.^2)') * param.Q',"fro")^2 + ...
-        sample_n(2) * norm(covmat(:,:,2) - (param.Q .* (param.D2.^2)') * param.Q',"fro")^2;
+    condn = size(covmat,3);
+    output = arrayfun(@(x)(sample_n(x) * norm(covmat(:,:,x) - (param.Q .* (param.D(:,x).^2)') * param.Q',"fro")^2),1:condn);
+    output = 1/4 * sum(output);
+
 end
 
 function [egrad] = egrad_func(param, covmat, sample_n)
     % this function is to calculate the gradient of the cost function.
-    temp_mat1 = (param.Q .* (param.D1.^2)');
-    temp_mat2 = (param.Q .* (param.D2.^2)');
-    temp_mat3 = covmat(:,:,1)*temp_mat1;
-    temp_mat4 = covmat(:,:,2)*temp_mat2;
+
+    condn = size(covmat,3);
+
+    temp_q = zeros([size(param.Q),condn]); 
+    temp_d = zeros(size(param.D)); 
+    for i = 1:condn
+        temp_mat = (param.Q .* (param.D(:,i).^2)');
+
+        % egrad for Q
+        temp_q(:,:,i) = - sample_n(i) * (covmat(:,:,i)*temp_mat - temp_mat * param.Q' * covmat(:,:,i)*temp_mat);
+
+        % egrad for D
+        temp_d(:,i) = - sample_n(i)*param.D(:,i).*(diag(param.Q' * covmat(:,:,i) * param.Q) - param.D(:,i).^2);
+    end
+
 
     egrad = struct(...
-        'Q',-4*sample_n(1)*(temp_mat3-temp_mat1*param.Q'*temp_mat3)-4*sample_n(2)*(temp_mat4-temp_mat2*param.Q'*temp_mat4),...
-        'D1', -4*sample_n(1)*param.D1.*(diag(param.Q' * covmat(:,:,1) * param.Q) - param.D1.^2),...
-        'D2', -4*sample_n(2)*param.D2.*(diag(param.Q' * covmat(:,:,2) * param.Q) - param.D2.^2));
+        'Q',sum(temp_q,3),...
+        'D', temp_d);
 
 end
